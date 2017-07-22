9597bd02eb21a2c2006bbc5f1cd0f3a1ed437164
siva6 <hohoonlee@siva6-MacBookPro.local>
2017-07-22 23:08:28 +0900

    ; trime  추가

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ let express = require('express');
 let presto  = require('../utils/presto');
 let router = express.Router();
 
-
 router.post('/tables', function(req, res, next) {
     presto.setOption(req.body.host, req.body.port, req.body.catalog, req.body.schema);
     presto.getTables(function(err, data, cols) {
@@ -15,7 +14,13 @@ router.post('/tables', function(req, res, next) {
 });
 
 router.post('/query', function(req, res, next) {
-    presto.query(req.body.q, function(err, data, cols) {
+    var q = req.body.q;
+    console.log(q, q.length);
+    q = q.trim();
+    if(q.lastIndexOf(';') === q.length-1) q = q.substring(0, q.length -1);
+    console.log(q, q.length);
+
+    presto.query(q, function(err, data, cols) {
         if(err) {
             res.status(500).json({'err':err});
         }else {
